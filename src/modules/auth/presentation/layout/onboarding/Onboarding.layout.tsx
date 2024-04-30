0fa08e4e8f778f16b40e42cbdd9c3c7d1ded06d6
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-30 14:49:16 +0200

    fix fonts import

--- src/modules/auth/presentation/layout/onboarding/Onboarding.layout.tsx
+++ src/modules/auth/presentation/layout/onboarding/Onboarding.layout.tsx
@@ -1,6 +1,10 @@
+import {Button} from "../../../../../ui/components/mainButton.tsx";
+
 const OnboardingLayout = () => {
     return (
         <>
+            <Button>Click me!</Button>
+            <h1 className={"h1"}>test</h1>
         </>
     );
 }
