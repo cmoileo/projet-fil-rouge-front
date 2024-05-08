413632d89b2c2e8ea13adc48e34d90cd23ec1ac6
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-05-08 12:11:18 +0200

    add relatif rendering for plus icon in nav items

--- src/ui/components/navItem.tsx
+++ src/ui/components/navItem.tsx
@@ -3,12 +3,16 @@ import {cn} from "../../services/shadcn/utils.ts";
 import {PlusIcon} from "lucide-react";
 
 
-export const NavItem = (props: { name: string, path: string, className: string }) => (
+export const NavItem = (props: { name: string, path: string, className: string; isPlusIcon: boolean }) => (
     <li className={cn(
         "p-m transition blue-1000 border-radius-100 padding-200-x cursor-pointer padding-100-y flex justify-between hover-bg-grey-200 items-center relative group",
         props.className
     )}>
         <Link to={props.path}>{props.name}</Link>
-        <PlusIcon className="transition opacity-0 group-hover:opacity-100" />
+        {
+        props.isPlusIcon && (
+            <PlusIcon className="hidden group-hover:block text-200" />
+        )
+    }
     </li>
 );
--- src/ui/layouts/Navbar.layout.tsx
+++ src/ui/layouts/Navbar.layout.tsx
@@ -8,21 +8,21 @@ export const Navbar = () => {
         {name: "Dashboard", path: "/dashboard/", className: ""},
     ]
     const navItems2 = [
-        {name: "Projects", path: "/dashboard/", className: ""},
-        {name: "Signup", path: "/dashboard/", className: ""},
-        {name: "Dashboard", path: "/dashboard/", className: ""},
+        {name: "Projects", path: "/dashboard/", className: "", isPlusIcon: true},
+        {name: "Signup", path: "/dashboard/", className: "", isPlusIcon: true},
+        {name: "Dashboard", path: "/dashboard/", className: "", isPlusIcon: true},
     ]
     getFolders();
     return (
         <nav className={"bg-grey-100 h-screen w-fit padding-200 flex flex-col gap-1000"}>
             <ul className={"flex flex-col w-60 gap-300"}>
                 {navItems1.map((item, index) => (
-                    <NavItem key={index} name={item.name} path={item.path} className={item.className}/>
+                    <NavItem key={index} name={item.name} path={item.path} className={item.className} isPlusIcon={false}/>
                 ))}
             </ul>
             <ul className={"flex flex-col w-60 gap-300"}>
                 {navItems2.map((item, index) => (
-                    <NavItem key={index} name={item.name} path={item.path} className={item.className}/>
+                    <NavItem key={index} name={item.name} path={item.path} className={item.className} isPlusIcon={item.isPlusIcon}/>
                 ))}
             </ul>
         </nav>
