4d0a93cbdc900e8651bc0ccc530fd58c0b45ddfd
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-05-08 11:04:57 +0200

    add protected route service

--- src/modules/protected/dashboard/presentation/page/Page.tsx
+++ src/modules/protected/dashboard/presentation/page/Page.tsx
@@ -1,4 +1,7 @@
+import {useProtectedRoute} from "../../../../../services/protectedRoute/ProtectedRoute.service.ts";
+
 export const DashboardPage = () => {
+    useProtectedRoute();
     return (
         <div>
             <h1>Dashboard</h1>
