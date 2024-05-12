260f0653a3e497d0092ede760de2ff4a88504b92
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-05-12 19:36:09 +0200

    improve popover UX

--- src/modules/protected/dashboard/emloyees/presentation/layout/add-employee/AddEmployee.layout.tsx
+++ src/modules/protected/dashboard/emloyees/presentation/layout/add-employee/AddEmployee.layout.tsx
@@ -1,13 +1,12 @@
 import {MainButton} from "../../../../../../../ui/components/mainButton.tsx";
 import {AddEmployeeViewModel} from "./AddEmployee.viewModel.tsx";
 import {Input} from "../../../../../../../ui/components/input.tsx";
-import {Popover, PopoverClose, PopoverContent, PopoverTrigger} from "@radix-ui/react-popover";
-
+import {Popover, PopoverContent, PopoverTrigger} from "@radix-ui/react-popover";
 export const AddEmployeeLayout = () => {
-    const {handleSubmit} = AddEmployeeViewModel();
+    const {handleSubmit, isOpen, setIsOpen} = AddEmployeeViewModel();
     return (
         <div>
-            <Popover>
+            <Popover open={isOpen} onOpenChange={setIsOpen}>
                 <PopoverTrigger asChild>
                     <MainButton children={"Add an employee"} />
                 </PopoverTrigger>
@@ -15,9 +14,7 @@ export const AddEmployeeLayout = () => {
                     <form onSubmit={handleSubmit} className={"w-full flex flex-col gap-400 padding-400 border-radius-300 z-10 shadow-[rgba(0,_0,_0,_0.25)_0px_0px_50px_0px]"}>
                         <Input id={"email"} type={"email"} placeholder={"Email"}/>
                         <Input id={"employeeRole"} type={"text"} placeholder={"Role"}/>
-                        <PopoverClose asChild>
-                            <MainButton children={"Add"}/>
-                        </PopoverClose>
+                        <MainButton type={"submit"} children={"Add"}/>
                     </form>
                 </PopoverContent>
             </Popover>
--- src/modules/protected/dashboard/emloyees/presentation/layout/add-employee/AddEmployee.viewModel.tsx
+++ src/modules/protected/dashboard/emloyees/presentation/layout/add-employee/AddEmployee.viewModel.tsx
@@ -1,20 +1,26 @@
 import {addEmployeeData} from "../../../../../../../repository/employee/add-employee.data.ts";
-import {FormEvent} from "react";
+import {FormEvent, useState} from "react";
 import {AddEmployeeDto} from "../../../domain/dto/add-employee.dto.ts";
 
 export const AddEmployeeViewModel = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
+        console.log('form submitted !')
         const target = e.target as HTMLFormElement
         if (!target) return;
+        setIsOpen(false)
         const data: AddEmployeeDto = {
             email: target.email.value,
-            role: target.employeeRole.value.length > 1 ? target.employeeRole.value : undefined
+            role: target.employeeRole.value.length > 1 ? target.employeeRole.value : null
         }
+        target.reset()
         await addEmployeeData(data);
     }
 
     return {
         handleSubmit,
+        isOpen,
+        setIsOpen,
     }
 }
