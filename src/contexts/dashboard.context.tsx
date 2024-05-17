7038f1c57d92814980f1014d988b2269115a4204
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-05-17 09:55:25 +0200

    improve projects and folders updates on navbar actions

--- src/contexts/dashboard.context.tsx
+++ src/contexts/dashboard.context.tsx
@@ -2,6 +2,8 @@ import {createContext, useEffect, useState} from "react";
 import {JobDto} from "../modules/protected/dashboard/emloyees/domain/dto/job.dto.ts";
 import {AccountType} from "../types/account/account.type.ts";
 import {getUserByIdData} from "../repository/account/get-user-by-id.data.ts";
+import {ProjectType} from "../types/project/projet.type.ts";
+import {getProjectsData} from "../repository/project/get-projects.data.ts";
 
 export const DashboardContext =
     createContext<{
@@ -9,17 +11,22 @@ export const DashboardContext =
         setJobs: React.Dispatch<React.SetStateAction<JobDto[]>>;
         account: AccountType | null;
         setAccount: React.Dispatch<React.SetStateAction<AccountType | null>>;
+        projects: ProjectType[] | null;
+        setProjects: React.Dispatch<React.SetStateAction<ProjectType[] | null>>;
     }>(
         {
             jobs: [],
             setJobs: () => {},
             account: null,
             setAccount: () => {},
+            projects: null,
+            setProjects: () => {},
         });
 
 export const DashboardProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [jobs, setJobs] = useState<JobDto[]>([]);
     const [account, setAccount] = useState<AccountType | null>(null);
+    const [projects, setProjects] = useState<ProjectType[] | null>(null);
 
     useEffect(() => {
         const getAccount = async(): Promise<void> => {
@@ -28,11 +35,18 @@ export const DashboardProvider: React.FC<{ children: React.ReactNode }> = ({ chi
                 setAccount(account);
             }
         }
+        const fetchProjects = async () => {
+            const projects = await getProjectsData()
+            if (projects) {
+                setProjects(projects)
+            }
+        }
+        fetchProjects();
         getAccount();
     }, []);
 
     return (
-        <DashboardContext.Provider value={{ jobs, setJobs, account, setAccount }}>
+        <DashboardContext.Provider value={{ jobs, setJobs, account, setAccount, projects, setProjects }}>
             {children}
         </DashboardContext.Provider>
     );
--- src/modules/protected/dashboard/projects/presentation/layout/projects.viewModel.tsx
+++ src/modules/protected/dashboard/projects/presentation/layout/projects.viewModel.tsx
@@ -1,21 +1,8 @@
-import {useEffect, useState} from "react";
-import {getProjectsData} from "../../../../../../repository/project/get-projects.data.ts";
-import {ProjectType} from "../../../../../../types/project/projet.type.ts";
+import {useContext} from "react";
+import {DashboardContext} from "../../../../../../contexts/dashboard.context.tsx";
 
 export const useProjects = () => {
-    const [projects, setProjects] = useState<ProjectType[] | null>(null)
-
-    useEffect(() => {
-        const fetchProjects = async () => {
-            const projects = await getProjectsData()
-            if (projects) {
-                setProjects(projects)
-            }
-        }
-
-        fetchProjects()
-    }, []);
-
+    const {projects} = useContext(DashboardContext);
     return {
         projects,
     }
