78ec2a2e88a7a54288de730a3f06478e2e9d2c5e
cmoileo <leo.fezard33@gmail.com>
2024-05-25 15:31:15 +0200

    handle create and update task category

--- src/modules/protected/dashboard/single-project/presentation/layout/task-category/task-category.layout.tsx
+++ src/modules/protected/dashboard/single-project/presentation/layout/task-category/task-category.layout.tsx
@@ -6,25 +6,30 @@ import {
 } from "../../../../../../../ui/components/dropdown-menu.tsx";
 import {MainButton} from "../../../../../../../ui/components/mainButton.tsx";
 import {useTaskCategory} from "./task-category.viewModel.tsx";
+import {SecondaryButton} from "../../../../../../../ui/components/secondaryButton.tsx";
+import {Input} from "../../../../../../../ui/components/input.tsx";
+import {Badge} from "../../../../../../../ui/components/badge.tsx";
 
 export const TaskCategoryLayout = () => {
-    const {taskCategories} = useTaskCategory();
+    const {taskCategories, isOpen, setIsOpen, createTaskInputRef, colorInputRef, handleCreateCategory } = useTaskCategory();
     return (
-        <DropdownMenu>
+        <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
             <DropdownMenuTrigger asChild>
                 <MainButton>Actions</MainButton>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem>
-                    Create Task Category
-                </DropdownMenuItem>
-                {
-                    taskCategories && taskCategories.map((taskCategory) => (
-                        <DropdownMenuItem key={taskCategory.id}>
-                            {taskCategory.name}
-                        </DropdownMenuItem>
-                    ))
-                }
+            <DropdownMenuContent className={"padding-300"}>
+                <div className="margin-300-bottom flex flex-col gap-300">
+                    {
+                        taskCategories && taskCategories.map((taskCategory) => (
+                            <DropdownMenuItem className={"p-0"} key={taskCategory.id}>
+                                <Badge className={"w-full padding-200-y cursor-pointer"} style={{backgroundColor: taskCategory.color}}>{taskCategory.name}</Badge>
+                            </DropdownMenuItem>
+                        ))
+                    }
+                </div>
+                    <Input ref={createTaskInputRef} type={"text"} placeholder={"Write new category"} />
+                    <Input ref={colorInputRef} className={"w-full margin-200-top"} type={"color"} />
+                    <SecondaryButton className={"p-xs w-full margin-200-top"} onClick={handleCreateCategory}>Create Task Category</SecondaryButton>
             </DropdownMenuContent>
         </DropdownMenu>
     );
--- src/modules/protected/dashboard/single-project/presentation/layout/task-category/task-category.viewModel.tsx
+++ src/modules/protected/dashboard/single-project/presentation/layout/task-category/task-category.viewModel.tsx
@@ -1,10 +1,40 @@
-import {useContext} from "react";
+import {useContext, useRef, useState} from "react";
 import {DashboardContext} from "../../../../../../../contexts/dashboard.context.tsx";
+import {createTaskCategoryData} from "../../../../../../../repository/task-categoriy/create-task-category.data.ts";
+import {getTaskCategoriesData} from "../../../../../../../repository/task/get-task-categories.data.ts";
 
 export const useTaskCategory = () => {
-    const {taskCategories} = useContext(DashboardContext);
+    const {taskCategories, setTaskCategories} = useContext(DashboardContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const createTaskInputRef: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
+    const colorInputRef: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
+
+    const handleCreateCategory = async () => {
+        if (!createTaskInputRef.current || !colorInputRef.current) return
+        const name = createTaskInputRef.current.value
+        const color = colorInputRef.current.value
+        const data = {
+            name,
+            color
+        }
+        createTaskInputRef.current.value = ''
+        colorInputRef.current.value = ''
+        await createTaskCategoryData(data)
+        updateTaskCategory()
+    }
+
+    const updateTaskCategory = async () => {
+        const updatedTaskCategory = await getTaskCategoriesData()
+        if (!updatedTaskCategory) return
+        setTaskCategories(updatedTaskCategory)
+    }
 
     return {
-        taskCategories
+        taskCategories,
+        isOpen,
+        setIsOpen,
+        createTaskInputRef,
+        handleCreateCategory,
+        colorInputRef
     }
 }
