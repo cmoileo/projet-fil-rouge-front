c22a0fe5d357efd1d81f73bd90a1679da6c9b998
cmoileo <leo.fezard33@gmail.com>
2024-06-04 10:38:56 +0200

    add occupation pt

--- src/modules/protected/dashboard/home/presentation/layout/employees-overview/employees-overview.layout.tsx
+++ src/modules/protected/dashboard/home/presentation/layout/employees-overview/employees-overview.layout.tsx
@@ -1,24 +1,32 @@
 import {useContext} from "react";
 import {DashboardContext} from "../../../../../../../contexts/dashboard.context.tsx";
-
+import {getEmployeeOccupationPtUseCase} from "../../../domain/use-case/get-employee-occupation-pt.useCase.ts";
 export const EmployeesOverviewLayout = () => {
     const { employees } = useContext(DashboardContext);
+    console.log(employees)
     return (
         <div className={"w-[43%] sticky top-4 h-fit"}>
             <h2 className={"h4"}>Employees overview</h2>
             <div className={"grid grid-cols-3 gap-10 margin-500-top justify-items-center"}>
                 {
                     employees?.map((employee) => (
-                        <div className={"flex flex-col items-center gap-400"} key={employee.id}>
-                            {
-                                employee.profile_picture_url ? (
-                                    <img className={"w-28 h-28 object-cover border-radius-full"} src={employee.profile_picture_url} alt={employee.firstname} />
-                                ) : (
-                                    <div className={"w-28 h-28 bg-gray-500 rounded-full"} />
-                                )
-                            }
-                            <p className={"p-m text-center"}>{employee.firstname}</p>
-                        </div>
+                        <div className={"flex flex-col items-center gap-400 h-full"} key={employee.id}>
+                            <div className={"flex flex-col justify-between items-center h-full w-full"}>
+                                {
+                                    employee.profile_picture_url ? (
+                                        <img className={"w-28 h-28 object-cover border-radius-full"}
+                                             src={employee.profile_picture_url} alt={employee.firstname}/>
+                                    ) : (
+                                        <div className={"w-28 h-28 bg-gray-500 rounded-full"}/>
+                                    )
+                                }
+                                <p className={"p-m text-center"}>{employee.firstname}</p>
+                            </div>
+                                <div className={"w-full h-2 border-radius-full bg-grey-200 relative"}>
+                                    <div style={{width: getEmployeeOccupationPtUseCase(employee) + "%"}} className={"h-full absolute left-0 bg-green-300 rounded-full"}>
+                                    </div>
+                                </div>
+                            </div>
                     ))
                 }
             </div>
