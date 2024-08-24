0892f6c8998e504b1cbc8ec337c51f974b274e91
cmoileo <leo.fezard33@gmail.com>
2024-08-24 15:25:29 +0200

    improve onboarding UI

--- src/modules/auth/presentation/layout/onboarding/Onboarding.layout.tsx
+++ src/modules/auth/presentation/layout/onboarding/Onboarding.layout.tsx
@@ -1,13 +1,28 @@
 import {MainButton} from "../../../../../ui/components/mainButton.tsx";
 import {SecondaryButton} from "../../../../../ui/components/secondaryButton.tsx";
 import { useNavigate } from 'react-router-dom';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import onBoardingImg from './onboarding-img.avif';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import appLogo from './app-logo.png';
 
 const OnboardingLayout = () => {
     const navigate = useNavigate();
     return (
-        <section className={"m-auto w-fit mt-72 gap-500 flex"}>
-            <MainButton onClick={() => {navigate("/signin")}}>Sign in</MainButton>
-            <SecondaryButton onClick={() => {navigate("/signup")}}>Sign up</SecondaryButton>
+        <section className={"w-full h-screen items-center gap-900 flex"}>
+            <img className={"w-1/2 h-full object-cover"} src={onBoardingImg} alt="Working employees around a table"/>
+            <div className={"flex w-1/3 bg-grey-100 padding-800 border-radius-500 flex-col"}>
+                <img src={appLogo} alt="App logo" className={"w-[70px] object-contain"}/>
+                <p className={"h6 margin-300-top"}>Connect with your team</p>
+                <p className="p-s">Centralize and manage all of your projects and their tasks, and improve your productivity and communication.</p>
+                <SecondaryButton className={"bg-grey-300 margin-500-top grey-800 w-full p-l"} onClick={() => {navigate("/signup")}}>Sign up</SecondaryButton>
+                <div className={'margin-300-top flex flex-col gap-300 items-center'}>
+                    <p className={"text-grey-700 p-xs"}>Already have an account?</p>
+                    <MainButton className={'w-full'} onClick={() => {navigate("/signin")}}>Sign in</MainButton>
+                </div>
+            </div>
         </section>
     );
 }
--- src/ui/components/secondaryButton.tsx
+++ src/ui/components/secondaryButton.tsx
@@ -4,11 +4,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "../../services/shadcn/utils.ts"
 
 const buttonVariants = cva(
-    "ease-in-out inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 p-s ",
+    "ease-in-out p-s inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
     {
         variants: {
             variant: {
-                default: "grey-400 hover-grey-900 bg-grey-100 hover-bg-grey-200 p-xs active-bg-grey-300",
+                default: "grey-700 hover-grey-900 bg-grey-100 hover-bg-grey-200 p-xs active-bg-grey-300",
                 destructive:
                     "bg-destructive text-destructive-foreground hover:bg-destructive/90",
                 outline:
@@ -21,7 +21,7 @@ const buttonVariants = cva(
             size: {
                 default: "h-10 px-4 py-2",
                 sm: "h-9 rounded-md px-3",
-                lg: "h-11 rounded-md px-8",
+                lg: "p-l",
                 icon: "h-10 w-10",
             },
         },
